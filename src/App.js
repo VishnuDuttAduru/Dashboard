965d5158811d9fb22e90e063aa2fc1155d8e8cb7
vishnuDevoloper <email2vscg@gmail.com>
2024-10-08 14:37:35 +0530

    General Layout Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./App.css";
+import LeftBar from './components/leftBar/LeftBar'
+import RightBar from './components/rightBar/RightBar'
+import Center from './components/center/Center'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='App'>
+      <LeftBar />
+      <Center />
+      <RightBar />
     </div>
   )
 }
---
+++ src/components/leftBar/LeftBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./LeftBar.css";
+
+const LeftBar = () => {
+    return (
+        <div className='left-bar'>
+            <h1>Left</h1>
+        </div>
+    )
+}
+
+export default LeftBar
